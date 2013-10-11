548c8caa424ed4cc757f25b79e65550b64c82fdc
Nayong Quan <nayong-quan@uiowa.edu>
2013-10-10 23:04:04 -0500

    Initial commit.

---
+++ Default.aspx.vb
@@ -0,0 +1,15 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Declaring the Variables for each field
+        Dim hoursWorked As Double
+        Dim wage As Double
+        Dim preTaxDeduction As Double
+        Dim postTaxDeduction As Double
+        Dim netIncome As Double
+
+        ' 
+    End Sub
+End Class
